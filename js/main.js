b9a6d870fee2f34e66679d124fe0b9c826558c8a
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-01-16 14:30:36 +0500

    Fix bug in tab panels for BEM

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item')
 const elsTabLink = document.querySelectorAll('.js-tab-link')
-const elsTabsPanel = document.querySelectorAll('.tabs__panel')
+const elsTabsPanel = document.querySelectorAll('.tabpanels__item')
 
 // Remover active item class
 function deActiviteElementItem(elements, className) {
@@ -20,13 +20,13 @@ elsTabLink.forEach(elTabLink => {
     // Add active class to current item
     elTabLink.parentElement.classList.add('tabs__item--active')
 
-    // Remove active class from tabs__panel elements
-    deActiviteElementItem(elsTabsPanel, 'tabs__panel--active')
+    // Remove active class from tabpanels__item elements
+    deActiviteElementItem(elsTabsPanel, 'tabpanels__item--active')
 
     // Show active tab panel
     // const activTabItems = document.querySelector(`#${elTabLink.href.split('#')[1]}`)
     const activTabItems = document.querySelector(elTabLink.dataset.tabTarget)
-    activTabItems.classList.add('tabs__panel--active')
+    activTabItems.classList.add('tabpanels__item--active')
     console.log(elTabLink.dataset);
   })
 })
